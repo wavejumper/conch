8fa890b4c33c4714ac2ec30aadc9fbe944e7e076
Damien Collard <damien.collard@laposte.net>
2017-02-17 11:25:05 +0100

    Bump version to 0.9.1.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject me.raynes/conch "0.9.0"
+(defproject me.raynes/conch "0.9.1"
   :license {:name "Eclipse Public License - v 1.0"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :url "https://github.com/Raynes/conch"
