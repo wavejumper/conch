9d0878ffcc54347afbe1ebf46d568281cfdcd77e
Arnaud Bos <arnaud@arnaudbos.com>
2019-03-09 00:51:31 +0100

    Prepare 0.9.2

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-commons/conch "0.9.1"
+(defproject clj-commons/conch "0.9.2-SNAPSHOT"
   :license {:name "Eclipse Public License - v 1.0"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :url "https://github.com/clj-commons/conj"
